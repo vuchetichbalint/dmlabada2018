8109c89f6304a0a162fefaf30cd4cd30ba7f694f
Balint Vuchetich <vuchetichbalint@gmail.com>
2018-11-27 13:15:54 +0100

    add holiday func

--- ada.py
+++ ada.py
@@ -1,14 +1,36 @@
 import allatok
 import pandas as pd
+from config import config
+
+import requests
 
 def add(x, y):
 	return x + y
 
+def get_holidays(country, year):
+	params = {
+		'country' : country,
+		'year': year,
+		'key': config.holiday_api_key
+	}	
+	response = requests.get(
+		'https://holidayapi.com/v1/holidays',
+		params=params
+		)
+	r = response.json()
+
+	return r
+
+
 
 if __name__ == '__main__':
+
+	print(get_holidays(country='HU', year=2017))
     # print(add(3,5))
-    allatok.kutya()
+    # allatok.kutya()
 
-    s = pd.Series([1,2,3,5,6])
+    # s = pd.Series([1,2,3,5,6])
 
-    print(s)
+    # print(s)
+
+    # print(config.secret)
